3f23b06f1831385092c7cd15014b13196127d5b8
Cole McKnight <cole.gclemson@renci.org>
2019-07-23 18:18:09 -0400

    pre merge

--- deploy/create-config-map.sh
+++ deploy/create-config-map.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # parse command-line arguments
-if [[ $# != 3 ]]; then
-	echo "usage: $0 <gke-project-id> <path-to-key-file> <cluster-id>"
+if [[ $# != 4 ]]; then
+	echo "usage: $0 <gke-project-id> <path-to-key-file> <cluster-id> <zone>"
 	exit -1
 fi
 
 PROJECTID="$1"
 KEYPATH="$2"
 CLUSTERID="$3"
+ZONE="$4"
 
-kubectl create configmap nextflow-server-config --from-literal=project.id=${PROJECTID} --from-file=project.key=${KEYPATH} --from-literal=project.cluster=${CLUSTERID}
+kubectl create configmap nextflow-server-config --from-literal=project.id=${PROJECTID} --from-file=project.key=${KEYPATH} --from-literal=project.cluster=${CLUSTERID} --from-literal=project.zone=${ZONE}
