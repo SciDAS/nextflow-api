c948bc9ffdbd125469f542212b9fa36f8ba32392
Cole McKnight <cbmckni@clemson.edu>
2019-11-07 12:41:08 -0800

    minor fix

--- kube-load.sh
+++ kube-load.sh
@@ -50,7 +50,7 @@ done
 # copy input data to pod
 echo "copying data..."
 
-kubectl exec $POD_NAME -- bash -c "mkdir -p $PVC_PATH/$USER"
+kubectl exec $POD_NAME -- bash -c "mkdir -p $PVC_PATH/$WORKFLOW_ID/$(basename $LOCAL_PATH)"
 kubectl cp "$LOCAL_PATH" "$POD_NAME:$PVC_PATH/$WORKFLOW_ID/$(basename $LOCAL_PATH)"
 
 # delete pod
