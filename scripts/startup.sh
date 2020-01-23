bf7bce8224aa8902bb5685e0dc89db3feaac59d8
Ben Shealy <bentshermann@gmail.com>
2020-01-23 17:48:09 -0500

    Added startup script for deployment

---
+++ scripts/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Startup script for kubernetes deployment.
+
+# start mongodb service
+service mongodb start
+
+# initialize backups directory
+BACKUPS="/workspace/_backups"
+
+mkdir -p ${BACKUPS}
+
+# restore database backup if present
+LATEST=$(ls ${BACKUPS}/* | tail)
+
+if [[ ! -z ${LATEST} ]]; then
+	./scripts/db-restore.sh "${BACKUPS}/${LATEST}"
+fi
+
+# start web server
+python3 server.py
