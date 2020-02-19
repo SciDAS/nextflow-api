0a1ee2170332aedac5bcb6e053a821211ebd726d
Ethan Bensman <ebenz99@gmail.com>
2020-02-18 20:01:09 -0500

    mongodb update

---
+++ database/scripts/db-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Backup a database to an archive.
+
+# parse command-line arguments
+if [[ $# != 1 ]]; then
+	>&2 echo "usage: $0 <type>"
+	exit 1
+fi
+
+DATABASE="wmongo"
+DUMP="dump"
+BACKUPS="/workspace/_backups"
+TYPE="$1"
+
+# remove existing dump directory
+rm -rf ${DUMP}
+
+# dump database to dump directory
+mongodump -d ${DATABASE} -o ${DUMP}
+
+# create archive of dump directory
+tar -czvf $(date +"${BACKUPS}/${TYPE}_%Y_%m_%d.tar.gz") ${DUMP}
+
+# remove older archives of the same type
+NUM_BACKUPS=$(ls ${BACKUPS}/${TYPE}_* | wc -l)
+MAX_BACKUPS=5
+
+if [[ ${NUM_BACKUPS} > ${MAX_BACKUPS} ]]; then
+	rm -f "$(ls ${BACKUPS}/${TYPE}_* | head -n 1)"
+fi
---
+++ database/scripts/startup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Startup script for kubernetes deployment.
+
+# # start mongodb service
+mkdir -p /data/db
+mkdir -p /logs
+touch /logs/log.txt
+mongod --fork --logpath /logs/log.txt --bind_ip 0.0.0.0
+
+# initialize backups directory
+BACKUPS="/workspace/_backups"
+
+mkdir -p ${BACKUPS}
+
+# restore database backup if present
+LATEST=$(ls ${BACKUPS} | tail -n 1)
+
+if [[ ! -z ${LATEST} ]]; then
+/scripts/db-restore.sh "${BACKUPS}/${LATEST}"
+fi
+
+#keeps for a little over a day
+sleep 100000
