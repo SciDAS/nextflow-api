3f5896d0bd57e244b1360834e6c6377c07b97ab6
Ben Shealy <bentshermann@gmail.com>
2020-02-27 19:10:06 -0500

    trying to fix database timeout issue

--- scripts/db-startup.sh
+++ scripts/db-startup.sh
@@ -2,7 +2,10 @@
 # Startup script for kubernetes deployment.
 
 # start mongodb service
-service mongodb start
+mkdir -p /data/db
+mkdir -p /logs
+touch /logs/log.txt
+mongod --fork --logpath /logs/log.txt --bind_ip 0.0.0.0
 
 # initialize backups directory
 BACKUPS="/workspace/_backups"
