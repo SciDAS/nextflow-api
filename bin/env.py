e27bf705b1d51ae80acd13124ec4804ac3d8ea5c
Ben Shealy <bentshermann@gmail.com>
2020-09-16 13:04:27 -0400

    Changed env.py to raise error if PVC_NAME is not defined

--- bin/env.py
+++ bin/env.py
@@ -1,14 +1,19 @@
 import os
 
+# define environment variables
 WORKFLOWS_DIRS = {
-	'k8s': '/workspace/_workflows',
-	'local': './_workflows',
-	'pbspro': './_workflows'
+	'k8s':    '/workspace/_workflows',
+	'local':  '_workflows',
+	'pbspro': '_workflows'
 }
 
 NXF_EXECUTOR = os.environ.get('NXF_EXECUTOR')
 NXF_EXECUTOR = NXF_EXECUTOR if NXF_EXECUTOR else 'local'
 
-PVC_NAME = os.environ.get('PVC_NAME', 'deepgtex-prp')
+PVC_NAME = os.environ.get('PVC_NAME')
 
 WORKFLOWS_DIR = WORKFLOWS_DIRS[NXF_EXECUTOR]
+
+# validate environment variables
+if NXF_EXECUTOR == 'k8s' and PVC_NAME is None:
+	raise EnvironmentError('Using k8s executor but PVC is not defined')
