0054c64498d1d4105c50f91e33a821f7bce5014b
Ben Shealy <bentshermann@gmail.com>
2021-09-30 17:18:16 -0400

    Changed startup-palmetto.sh to use mongo backend

--- scripts/startup-palmetto.sh
+++ scripts/startup-palmetto.sh
@@ -8,7 +8,16 @@ module load nextflow/20.07.1
 # initialize environment
 source activate nextflow-api
 
+# start mongodb server
+killall mongod
+
+mongod \
+    --fork \
+    --dbpath /mongo/${USER}/data \
+    --logpath /mongo/${USER}/mongod.log \
+    --bind_ip_all
+
 # start web server
 export NXF_EXECUTOR="pbspro"
 
-python bin/server.py --backend=file
+python bin/server.py --backend=mongo
